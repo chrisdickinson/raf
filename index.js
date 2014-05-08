4f7d03645382aa8a2939a573a12546ac5f5cd936
Christian Tegnér <christian.tegner@gmail.com>
2014-05-08 15:52:30 +0200

    Move polyfill to index.js
    This change is backwards incompatible and changes this module from a
    requestAnimationFrame event-emitter to a requestAnimationFrame polyfill

--- index.js
+++ index.js
@@ -1,50 +1,65 @@
-module.exports = raf
+var now = require('performance-now')
+  , global = typeof window === 'undefined' ? {} : window
+  , vendors = ['ms', 'moz', 'webkit', 'o']
+  , suffix = 'AnimationFrame'
+  , raf = global['request' + suffix]
+  , caf = global['cancel' + suffix] || global['cancelRequest' + suffix]
 
-var EE = require('events').EventEmitter
-  , _raf = require('./polyfill.js')
-  , now = global.performance && global.performance.now ? function() {
-    return performance.now()
-  } : Date.now || function () {
-    return +new Date()
-  }
+for(var i = 0; i < vendors.length && !raf; i++) {
+  raf = global[vendors[i] + 'Request' + suffix]
+  caf = global[vendors[i] + 'Cancel' + suffix]
+      || global[vendors[i] + 'CancelRequest' + suffix]
+}
 
+if(!raf) {
+  var last = 0
+    , id = 0
+    , queue = []
+    , frameDuration = 1000 / 60
 
-function raf(el, tick) {
-  var now = raf.now()
-    , ee = new EE
-    
-  if(typeof el === 'function') {
-    tick = el
-    el = undefined
-  }
-  
-  ee.pause = function() { ee.paused = true }
-  ee.resume = function() { ee.paused = false }
-
-  _raf.call(window, iter, el)
-  
-  if(tick) {
-    ee.on('data', function(dt) {
-      tick(dt)
-    })
-  }
-
-  return ee
-
-  function iter(timestamp) {
-    var _now = raf.now()
-      , dt = _now - now
-    
-    now = _now
-
-    if(!ee.paused) {
-      ee.emit('data', dt)
+  raf = function(callback) {
+    if(queue.length === 0) {
+      var _now = now()
+        , next = Math.max(0, frameDuration - (_now - last))
+      last = next + _now
+      setTimeout(function() {
+        var cp = queue.slice(0)
+        // Clear queue here to prevent
+        // callbacks from appending listeners
+        // to the current frame's queue
+        queue.length = 0
+        for (var i = 0; i < cp.length; i++) {
+          if (!cp[i].cancelled) {
+            try{
+              cp[i].callback(last)
+            } catch(e) {}
+          }
+        }
+      }, next)
     }
-    
-    _raf.call(window, iter, el)
+    queue.push({
+      handle: ++id,
+      callback: callback,
+      cancelled: false
+    })
+    return id
+  }
+
+  caf = function(handle) {
+    for(var i = 0; i < queue.length; i++) {
+      if(queue[i].handle === handle) {
+        queue[i].cancelled = true
+      }
+    }
   }
 }
 
-raf.polyfill = _raf
-raf.now = now
-
+module.exports = function() {
+  // Wrap in a new function to prevent
+  // `cancel` potentially being assigned
+  // to the native rAF function
+  return raf.apply(global, arguments)
+}
+module.exports.cancel = function() {
+  caf.apply(global, arguments)
+}
